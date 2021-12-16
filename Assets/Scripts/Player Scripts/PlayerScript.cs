5048014101f1ea803a827dfb6b5036566ac590a2
IbrahimHamwi <ibrahimhamwi_619@hotmail.com>
2021-12-16 18:38:11 +0300

    Attack and Jump Animations added

--- Assets/Scripts/Player Scripts/PlayerScript.cs
+++ Assets/Scripts/Player Scripts/PlayerScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:efa4a505808e870218a5470ca994f9c323db4609cbfa808cd454dbcf1fafeacc
-size 2652
+oid sha256:603b57ea2b1a0fdb25f7cc6da11c0b871cd57617947ca2f0c0bf3761d8822275
+size 3516
