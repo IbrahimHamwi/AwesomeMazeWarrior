1b04bf019e060e83c99de54a96671eeae39c04b6
IbrahimHamwi <ibrahimhamwi_619@hotmail.com>
2022-01-18 21:33:17 +0300

    Door opening and closing

---
+++ Assets/Scripts/Enemy Scripts/EnemyDamage.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5bbc0edb67f7706c280050aabf18b5ba2bb24f9fa6c2cd87fc5ea8f8f8196222
+size 618
---
+++ Assets/Scripts/Player Scripts/PlayerScript.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:81155232c8a8ee8850411f8c4583d6fa37be70ce72a8f1b43ee6dc89617ea7a7
+size 4361
